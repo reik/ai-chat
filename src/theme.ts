5626aee10334f95a113f405f7d564bfd710c5997
Rei Kurata <kurarei@gmail.com>
2026-03-01 13:08:11 -0800

    adding material-ui and its theme

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#50C878',
+    },
+    secondary: {
+      main: '#ffd966',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
+
+export default theme;
